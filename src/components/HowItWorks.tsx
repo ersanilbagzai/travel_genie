74325010faa05800112f1d44af54102eeb858b4f
ersanilbagzai <er.sanil.bagzai@gmail.com>
2025-09-06 20:08:36 +0530

    Updated HowItWorks.tsx

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -17,7 +17,7 @@ const HowItWorks: React.FC = () => {
           </div>
           <div className="hidden md:block">
             <img 
-              src="src/assets/images/hot_it_works_image.webp" 
+              src="src/assets/images/hot_it_works_image.png" 
               alt="Happy traveler" 
               className="rounded-2xl shadow-xl"
               width="600"
